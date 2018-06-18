1e996d7d664faaf44ca9a605154f14e6567ebefa
Oleh <pio_ua@ukr.net>
2018-06-18 15:25:14 +0300

    1

--- src/main/java/org/olezha/Staircase.java
+++ src/main/java/org/olezha/Staircase.java
@@ -9,6 +9,8 @@ public class Staircase {
         int n = scanner.nextInt();
         scanner.close();
         staircase(n);
+
+        System.out.println(timeConversion("07:05:45PM"));
     }
 
     static void staircase(int n) {
@@ -17,5 +19,22 @@ public class Staircase {
             out += "#";
             System.out.printf("%" + n + "s%n", out);
         }
+    }
+
+    static String timeConversion(String s) {
+        String time = s.substring(0, 8);
+        String flag = s.substring(8);
+
+        int hour = Integer.parseInt(time.substring(0, 2));
+        String secondPart = time.substring(2);
+        if (hour == 12)
+            hour = 0;
+
+        if ("PM".equals(flag)) {
+            hour += 12;
+            if (hour == 24)
+                hour = 0;
+        }
+        return String.format("%02d", hour) + secondPart;
     }
 }
