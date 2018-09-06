ba13bbf9283cdf70bf7c6d2545eb5890cebdd417
Oleh Shklyar <oleh.shklyar@gmail.com>
2018-09-07 01:00:20 +0300

    Testcase 0

--- src/main/java/org/olezha/FavoriteSequence.java
+++ src/main/java/org/olezha/FavoriteSequence.java
@@ -2,15 +2,14 @@ package org.olezha;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Locale;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.*;
 
 public class FavoriteSequence {
 
     /**
      * https://www.hackerrank.com/challenges/favourite-sequence
+     * http://chasethered.com/2014/11/my-problem-for-hackerrank-weekly-challenge-12-favorite-sequence/
      */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
@@ -36,12 +35,57 @@ public class FavoriteSequence {
     }
 
     static int[] lessSequence(int[][] nCopiesOfBrokenSequenceM) {
-        /* TODO
-         * - найти все совпадающие цифры
-         * - собрать последовательность сразу в лексикографическом порядке,
-         *   исходя из найденных интервалов
-         */
-        return null;
+        NodesSet occurrences = new NodesSet();
+        for (int[] brokenM : nCopiesOfBrokenSequenceM) {
+            Node previousNode = null;
+            for (int x : brokenM) {
+                Node node = occurrences.getOrPut(x);
+                if (previousNode != null)
+                    previousNode.nextNode = node;
+                previousNode = node;
+            }
+        }
+        return occurrences.toArray();
+    }
+}
+
+class Node {
+
+    int x;
+    Node nextNode;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Node a = (Node) obj;
+
+        return a.x == x;
+    }
+
+    @Override
+    public int hashCode() {
+        return x % 10;
+    }
+}
+
+class NodesSet {
+
+    private Map<Node, Node> map = new HashMap<>();
+
+    Node getOrPut(int i) {
+        Node node = new Node() {{
+            x = i;
+        }};
+
+        map.putIfAbsent(node, node);
+
+        return node;
+    }
+
+    int[] toArray() {
+        return map.values().stream().mapToInt(node -> node.x).toArray();
     }
 }
 
@@ -49,7 +93,7 @@ public class FavoriteSequence {
 class Main2 {
 
     public static void main(String[] args) {
-        log.info("{}", FavoriteSequence.lessSequence(new int[][]{
+        log.info("should 1 2 3 4: {}", FavoriteSequence.lessSequence(new int[][]{
                 {1, 3},
                 {2, 3, 4}}));
     }
