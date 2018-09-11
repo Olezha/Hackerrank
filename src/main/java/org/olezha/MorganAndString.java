a19175e32a4f9656e0edeb9307ad50dc8b14d89c
Oleh <pio_ua@ukr.net>
2018-09-11 19:18:18 +0300

    solved

--- src/main/java/org/olezha/MorganAndString.java
+++ src/main/java/org/olezha/MorganAndString.java
@@ -6,8 +6,6 @@ package org.olezha;
 class MorganAndString {
 
     static String morganAndString(String a, String b) {
-
-        // char arr around 40% faster then working with Str
         char[] aLetters = a.toCharArray();
         char[] bLetters = b.toCharArray();
 
@@ -15,43 +13,34 @@ class MorganAndString {
                 new StringBuilder(aLetters.length + bLetters.length);
         int aLettersPointer = 0;
         int bLettersPointer = 0;
+
         lettersOneByOne:
         while (aLetters.length > aLettersPointer || bLetters.length > bLettersPointer)
             if (aLetters.length == aLettersPointer) {
-                lexicographicallyMinStrFromTwoCollections.append(b.substring(bLettersPointer));
+                while (bLettersPointer < bLetters.length)
+                    lexicographicallyMinStrFromTwoCollections.append(bLetters[bLettersPointer++]);
                 break;
-            }
-            else if (bLetters.length == bLettersPointer) {
-                lexicographicallyMinStrFromTwoCollections.append(a.substring(aLettersPointer));
+            } else if (bLetters.length == bLettersPointer) {
+                while (aLettersPointer < aLetters.length)
+                    lexicographicallyMinStrFromTwoCollections.append(aLetters[aLettersPointer++]);
                 break;
-            }
-
-            else if (aLetters[aLettersPointer] < bLetters[bLettersPointer])
+            } else if (aLetters[aLettersPointer] < bLetters[bLettersPointer])
                 lexicographicallyMinStrFromTwoCollections.append(aLetters[aLettersPointer++]);
             else if (aLetters[aLettersPointer] > bLetters[bLettersPointer])
                 lexicographicallyMinStrFromTwoCollections.append(bLetters[bLettersPointer++]);
 
             else /* aLetters[aLettersPointer] == bLetters[bLettersPointer] */ {
-                if (aLetters.length == aLettersPointer + 1) {
-                    lexicographicallyMinStrFromTwoCollections.append(bLetters[bLettersPointer++]);
-                    continue;
-                }
-                if (bLetters.length == bLettersPointer + 1) {
-                    lexicographicallyMinStrFromTwoCollections.append(aLetters[aLettersPointer++]);
-                    continue;
-                }
-
                 int innerALettersPointer = aLettersPointer + 1;
                 int innerBLettersPointer = bLettersPointer + 1;
                 while (aLetters.length > innerALettersPointer || bLetters.length > innerBLettersPointer) {
                     if (aLetters.length == innerALettersPointer) {
-                        lexicographicallyMinStrFromTwoCollections.append(bLetters[innerBLettersPointer]);
-                        continue;
+                        lexicographicallyMinStrFromTwoCollections.append(bLetters[bLettersPointer++]);
+                        continue lettersOneByOne;
                     }
 
                     if (bLetters.length == innerBLettersPointer) {
-                        lexicographicallyMinStrFromTwoCollections.append(aLetters[innerALettersPointer]);
-                        continue;
+                        lexicographicallyMinStrFromTwoCollections.append(aLetters[aLettersPointer++]);
+                        continue lettersOneByOne;
                     }
 
                     if (aLetters[innerALettersPointer] == bLetters[innerBLettersPointer]) {
@@ -66,8 +55,7 @@ class MorganAndString {
                         while (innerALettersPointer > aLettersPointer
                                 && letter >= aLetters[innerALettersPointer])
                             lexicographicallyMinStrFromTwoCollections.append(aLetters[aLettersPointer++]);
-                    }
-                    else {
+                    } else {
                         char letter = bLetters[bLettersPointer++];
                         lexicographicallyMinStrFromTwoCollections.append(letter);
                         while (innerBLettersPointer > bLettersPointer
@@ -78,11 +66,7 @@ class MorganAndString {
                     continue lettersOneByOne;
                 }
 
-//                lexicographicallyMinStrFromTwoCollections.append(a.substring(aLettersPointer)); // OutOfMemoryError: Java heap space
-                while (aLettersPointer < aLetters.length)
-                    lexicographicallyMinStrFromTwoCollections.append(aLetters[aLettersPointer++]);
-                while (bLettersPointer < bLetters.length)
-                    lexicographicallyMinStrFromTwoCollections.append(bLetters[bLettersPointer++]);
+                lexicographicallyMinStrFromTwoCollections.append(aLetters[aLettersPointer++]);
             }
 
         return lexicographicallyMinStrFromTwoCollections.toString();
